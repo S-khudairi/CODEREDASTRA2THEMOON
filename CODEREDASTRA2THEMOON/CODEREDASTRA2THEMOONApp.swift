56bb6417e7368b34f3e5f3e3d17c8e83c16fad00
S-khudairi <Samkhudairi2002@gmail.com>
2025-10-18 15:25:16 -0500

    Initial Commit

---
+++ CODEREDASTRA2THEMOON/CODEREDASTRA2THEMOONApp.swift
@@ -0,0 +1,17 @@
+//
+//  CODEREDASTRA2THEMOONApp.swift
+//  CODEREDASTRA2THEMOON
+//
+//  Created by Sam Khudairi on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CODEREDASTRA2THEMOONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CODEREDASTRA2THEMOON/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CODEREDASTRA2THEMOON
+//
+//  Created by Sam Khudairi on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
